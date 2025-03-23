195a8a73b0458e574d7cacf8575adffb0731a4dc
ikar888 <ikar_888@yahoo.com>
2025-03-23 02:52:16 -0400

    complete

--- server.py
+++ server.py
@@ -4,8 +4,8 @@
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, request, render_template
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
-from flask import Flask, request, render_template, url_for, redirect
 
 #Initiate the flask app : TODO
 app = Flask("Sentiment Analyzer")
@@ -14,16 +14,26 @@ app = Flask("Sentiment Analyzer")
 def sent_analyzer():
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
+
     # Pass the text to the sentiment_analyzer function and store the response
     response = sentiment_analyzer(text_to_analyze)
+
     # Extract the label and score from the response
     label = response['label']
     score = response['score']
-    # Return a formatted string with the sentiment label and score
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
+    # Check if the label is None, indicating an error or invalid input
+    if label is None:
+        return "Invalid input! Try again."
+    else:
+        # Return a formatted string with the sentiment label and score
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
+# Renders the HTML page
 @app.route("/")
+# Renders the HTML page
 def render_index_page():
+    # Renders the HTML page
     return render_template('index.html')
 
 if __name__ == "__main__":
